0f6c60408291e5785189c9ac26010557cd830210
Genius-code <moh.aly8890@gmail.com>
2024-03-10 12:27:27 +0200

    update_classes_and_trainings

--- resources/views/Academy/pages/clasess/partials/_form.blade.php
+++ resources/views/Academy/pages/clasess/partials/_form.blade.php
@@ -37,21 +37,22 @@
             <span class="text-danger">*{{$message}}</span>
             @enderror
         </div>
+        <div class="col-md-6 mb-3">
+            <label for="classes"><span class="text-danger">*</span> {{trans('admin.training.training')}} </label>
+            <select id="classes" class="form-select pt-2" name="training_id" >
+                <option value=""> {{trans('admin.clasess.select_training')}} </option>
+                @foreach($trainings as $training)
+                    <option value="{{$training->id}}" @selected(old('training_id',  (isset($class) ? $class->training_id : '')) == $training->id) >{{$training->name}}</option>
+                @endforeach
+            </select>
+            @error('training_id')
+            <span class="text-danger">*{{$message}}</span>
+            @enderror
+        </div>
     </div>
-    <div class="col-md-6 mb-3">
-        <label for="classes"><span class="text-danger">*</span> {{trans('admin.training.training')}} </label>
-        <select id="classes" class="form-select pt-2" name="training_id" >
-            <option value=""> {{trans('admin.clasess.select_training')}} </option>
-            @foreach($trainings as $training)
-                <option value="{{$training->id}}" @selected(old('training_id',  (isset($class) ? $class->training_id : '')) == $training->id) >{{$training->name}}</option>
-            @endforeach
-        </select>
-        @error('training_id')
-        <span class="text-danger">*{{$message}}</span>
-        @enderror
-    </div>
+
 </div>
-@if(request()->isMethod('POST'))
+@if(request()->url() === route('academy.class.create'))
 <div class="row">
     <div class="col-md-6 mb-3" id="outcomes-container">
         <label for="outcomes">{{ trans('admin.clasess.out_comes') }}</label>
@@ -84,7 +85,8 @@
 
 </div>
 @else
-    <div class="row">
+    @isset($class)
+        <div class="row">
 
             <div class="col-md-6 mb-3" id="outcomes-container">
                 @php
@@ -106,27 +108,29 @@
                     @enderror
                 @endfor
             </div>
-        <div class="col-md-6 mb-3" id="bring-with-me-container">
-            @php
-                $bring_with_me = json_decode($class->bring_with_me, true);
-                $numberBringsWithMe = is_array($bring_with_me) ? count($bring_with_me) : 0;
-            @endphp
-            @for($i = 0; $i < $numberBringsWithMe; $i++)
-                <label for="bring_with_me">{{ trans('admin.clasess.bring_with_me') }}</label>
-                <!-- Initial input field -->
-                <div class="input-group mb-2">
-                    <input class="form-control bring-with-me-input" type="text" name="bring_with_me[]" value="{{ $bring_with_me[$i] }}" id="bring_with_me">
-                    <div class="input-group-append">
-                        <button class="btn btn-danger remove-bring-with-me" type="button">-</button>
+            <div class="col-md-6 mb-3" id="bring-with-me-container">
+                @php
+                    $bring_with_me = json_decode($class->bring_with_me, true);
+                    $numberBringsWithMe = is_array($bring_with_me) ? count($bring_with_me) : 0;
+                @endphp
+                @for($i = 0; $i < $numberBringsWithMe; $i++)
+                    <label for="bring_with_me">{{ trans('admin.clasess.bring_with_me') }}</label>
+                    <!-- Initial input field -->
+                    <div class="input-group mb-2">
+                        <input class="form-control bring-with-me-input" type="text" name="bring_with_me[]" value="{{ $bring_with_me[$i] }}" id="bring_with_me">
+                        <div class="input-group-append">
+                            <button class="btn btn-danger remove-bring-with-me" type="button">-</button>
+                        </div>
                     </div>
-                </div>
-                <button id="add-bring-with-me" type="button" class="btn btn-primary">{{ trans('admin.clasess.add_more') }}</button>
-                @error('bring_with_me.*')
-                <span class="text-danger">*{{$message}}</span>
-                @enderror
-            @endfor
+                    <button id="add-bring-with-me" type="button" class="btn btn-primary">{{ trans('admin.clasess.add_more') }}</button>
+                    @error('bring_with_me.*')
+                    <span class="text-danger">*{{$message}}</span>
+                    @enderror
+                @endfor
+            </div>
         </div>
-    </div>
+    @endisset
+
 @endif
 
 
