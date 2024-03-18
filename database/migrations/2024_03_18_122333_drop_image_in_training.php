ee3928189264bc22c4665af07f78bb816835a767
Genius-code <moh.aly8890@gmail.com>
2024-03-18 14:34:36 +0200

    drop_image

--- database/migrations/2024_03_18_122333_drop_image_in_training.php
+++ database/migrations/2024_03_18_122333_drop_image_in_training.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('training', function (Blueprint $table) {
+        Schema::table('trainings', function (Blueprint $table) {
             $table->dropColumn('image');
         });
     }
