8c4078564a493855b0f00014f44664db7a4fd7c0
Genuis-code <moh.aly8890@gmail.com>
2024-07-13 13:01:01 +0300

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -24,7 +24,7 @@ class ProfileController extends Controller
     public function update(Academies $user , ProfileRequest $request)
     {
         $imageName = $request->hasFile('logo') ? $this->upload($request->file('logo') , $this->academies::PATH,  $user->getRawOriginal('logo')) : $user->getRawOriginal('logo');
-        dd($imageName);
+        dd($user);
         $user->update([
             'commercial_name'=>$request->name,
             'logo'=> $imageName,
