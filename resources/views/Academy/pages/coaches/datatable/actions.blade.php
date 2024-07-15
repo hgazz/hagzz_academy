e087f4f5ea5226f61eb986c61e57f5d7109292a8
ali <ali@example.com>
2024-07-15 14:14:45 +0300

    update-table-lang

--- resources/views/Academy/pages/coaches/datatable/actions.blade.php
+++ resources/views/Academy/pages/coaches/datatable/actions.blade.php
@@ -1,6 +1,6 @@
 <td>
     <div class="btn-group  mb-2 me-4" role="group">
-        <button id="btndefault" type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ trans('admin.actions') }} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></button>
+        <button id="btndefault" type="button" class="btn btn-dark dropdown-toggle d-flex align-items-center justify-content-between" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ trans('admin.actions') }} <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg></button>
         <div class="dropdown-menu" aria-labelledby="btndefault">
             <a class="dropdown-item" href="{{route('academy.coach.edit', $coach)}}">{{ trans('admin.edit') }}</a>
 {{--            <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">--}}
