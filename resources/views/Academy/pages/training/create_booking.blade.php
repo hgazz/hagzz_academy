1d53fd595b4b0a11ee1a3642a3f8e57f7e5af5e3
Genuis-code <moh.aly8890@gmail.com>
2024-09-04 23:55:55 +0300

    fix_bugs

--- resources/views/Academy/pages/training/create_booking.blade.php
+++ resources/views/Academy/pages/training/create_booking.blade.php
@@ -105,7 +105,7 @@
                                 <div class="col-md-6 mb-3">
                                     <label for="country">{{ trans('admin.training.country') }}</label>
                                     <select class="form-select" id="country" name="country_id">
-                                        <option value="0">{{ trans('admin.training.Select County') }}</option>
+                                        <option value="">{{ trans('admin.training.Select County') }}</option>
                                         @foreach($countries as $country)
                                             <option
                                                 value="{{ $country->id }}" @selected(old('country_id'))>{{ $country->name }}</option>
