7c7623a6c4e104bc152b3510985e9dd761961c44
Genuis-code <moh.aly8890@gmail.com>
2025-01-19 23:48:42 +0200

    update_training

--- resources/views/Academy/pages/joins/details.blade.php
+++ resources/views/Academy/pages/joins/details.blade.php
@@ -51,7 +51,7 @@
                                         <li class="nav-item" role="presentation">
                                             <button class="nav-link" id="details-tab-icon" data-bs-toggle="tab" data-bs-target="#details-tab-icon-pane" type="button" role="tab" aria-controls="details-tab-icon-pane" aria-selected="true">
 
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>{{ trans('admin.user_details') }}
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-list"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>{{ trans('admin.user.user_details') }}
                                             </button>
                                         </li>
                                     </ul>
