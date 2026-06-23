c90981ad5a14dfca4ac3cf6d7aead53520816a09
Moh-saudi <mo.saudi19@gmail.com>
2026-06-23 20:12:19 +0300

    Fix academy asset console errors

--- public/assetsAdmin/yajaraLog.js
+++ public/assetsAdmin/yajaraLog.js
@@ -1,17 +1,26 @@
 // Disable DataTables error prompt
-$.fn.dataTable.ext.errMode = 'none';
+if (window.jQuery && $.fn && $.fn.dataTable && $.fn.dataTable.ext) {
+    $.fn.dataTable.ext.errMode = 'none';
+}
 
-$(document).ajaxError(function(event, jqxhr, settings, exception) {
-    if (exception === 'Unauthorized') {
+if (window.jQuery) {
+    $(document).ajaxError(function(event, jqxhr, settings, exception) {
+        if (jqxhr.status === 401 || exception === 'Unauthorized') {
+            var langMeta = document.querySelector('meta[name="lang"]');
+            var locale = langMeta ? langMeta.getAttribute('content') : 'en';
+            var logoutUrl = '/' + locale + '/partner/logout';
+
         // Prompt user if they'd like to be redirected to the login page
-        bootbox.confirm("Your session has expired. Would you like to be redirected to the login page?", function(result) {
-            if (result) {
-                // Update the URL to the correct path
-                window.location.href = 'https://partner.hagzz.com/partner/logout';
+            if (window.bootbox) {
+                bootbox.confirm("Your session has expired. Would you like to be redirected to the login page?", function(result) {
+                    if (result) {
+                        window.location.href = logoutUrl;
+                    }
+                });
+            } else {
+                window.location.href = logoutUrl;
             }
-        });
-    }
-});
-
-
+        }
+    });
+}
 
